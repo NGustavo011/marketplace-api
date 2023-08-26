cdcaced61e1b43fd0a9d97d8fcb4a2e85cb4d5be
NGustavo011 <gsn181200@gmail.com>
2023-08-26 18:26:56 -0300

    docs: add api-key in docs

--- src/main/docs/swagger/components/swagger-components.ts
+++ src/main/docs/swagger/components/swagger-components.ts
@@ -1,3 +1,4 @@
+import { apiKeyAuthSchema } from '../schemas/user/api-key-auth';
 import { badRequest } from './responses/bad-request';
 import { forbidden } from './responses/forbidden';
 import { notFound } from './responses/not-found';
@@ -5,6 +6,9 @@ import { serverError } from './responses/server-error';
 import { unauthorized } from './responses/unauthorized';
 
 export const swaggerComponents = {
+	securitySchemes: {
+		apiKeyAuth: apiKeyAuthSchema
+	},
 	badRequest,
 	unauthorized,
 	serverError,
